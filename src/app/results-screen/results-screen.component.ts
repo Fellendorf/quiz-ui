6b4f93c37cb6851592f998bf5c3c6f6bc9b8fea9
Fellendorf <finn85fellendorf@gmail.com>
2024-10-19 02:06:37 +0300

    Result screen:

--- src/app/results-screen/results-screen.component.ts
+++ src/app/results-screen/results-screen.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterLink } from '@angular/router';
 
 import { QuizService } from '../core/quiz.service';
 import { CodeComponent } from '../shared/code/code.component';
+import { Question } from '../models';
 
 @Component({
   selector: 'app-results-screen',
@@ -16,15 +17,32 @@ export class ResultsScreenComponent {
   private readonly router = inject(Router);
   public readonly quizService = inject(QuizService);
 
+  public index = 0;
+  public question!: Question;
+
   public ngOnInit() {
     if (!this.quizService.questions) {
       this.router.navigateByUrl('/');
     }
+    this.question = this.quizService.questions[this.index];
   }
 
   public getNumberCorrectAnswers(): number {
     return this.quizService.questions.filter(
       (question) => question.answer.index === question.userAnswer,
     ).length;
+  }
+
+  public setQuestion(index: number): void {
+    this.index = index;
+    this.question = this.quizService.questions[index];
+  }
+
+  public setColor(question: Question) {
+    return question.answer.index === question.userAnswer ? 'green' : 'red';
+  }
+
+  public isQuestionChecked(index: number): boolean {
+    return this.index === index;
   }
 }
