4396f683955d36ecf53c3cc8472317079fc49c48
Fellendorf <finn85fellendorf@gmail.com>
2024-10-22 00:18:30 +0300

    Routes: enum of pathes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,23 @@ import { MenuScreenComponent } from './menu-screen/menu-screen.component';
 import { QuizScreenComponent } from './quiz-screen/quiz-screen.component';
 import { ResultsScreenComponent } from './results-screen/results-screen.component';
 
+export enum ROUTE_PATHES {
+  MENU = '',
+  QUIZ = 'quiz',
+  RESULTS = 'results',
+}
+
 export const routes: Routes = [
   {
-    path: '',
+    path: ROUTE_PATHES.MENU,
     component: MenuScreenComponent,
   },
   {
-    path: 'quiz',
+    path: ROUTE_PATHES.QUIZ,
     component: QuizScreenComponent,
   },
   {
-    path: 'results',
+    path: ROUTE_PATHES.RESULTS,
     component: ResultsScreenComponent,
   },
 ];
