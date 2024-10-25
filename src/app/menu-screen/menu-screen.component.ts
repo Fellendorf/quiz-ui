5b45730ece893358c719b10c7242f3edf674eec3
Fellendorf <finn85fellendorf@gmail.com>
2024-10-26 01:10:45 +0300

    Menu Screen: refactoring, new link to admin questions screen,

--- src/app/menu-screen/menu-screen.component.ts
+++ src/app/menu-screen/menu-screen.component.ts
@@ -1,19 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, UpperCasePipe } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
-import {
-  LoadingState,
-  toLoadingStateStream,
-} from '../shared/loading-state/loading-state';
+import { toLoadingStateStream } from '../shared/loading-state/loading-state';
 import { ApiService } from '../core/api.service';
 import { QuizService } from '../core/quiz.service';
+import { AuthService } from '../core/auth.service';
 import { LoadingScreenComponent } from '../shared/loading-screen/loading-screen.component';
 import { TopicOptionsComponent } from './topic-options/topic-options.component';
 import { CountOptionsComponent } from './count-options/count-options.component';
-import { Topic } from '../models';
 import { ROUTE_PATHES } from '../app.routes';
+import { Topic } from '../models';
 
 @Component({
   selector: 'app-menu-screen',
@@ -21,6 +19,7 @@ import { ROUTE_PATHES } from '../app.routes';
   imports: [
     RouterLink,
     AsyncPipe,
+    UpperCasePipe,
     LoadingScreenComponent,
     TopicOptionsComponent,
     CountOptionsComponent,
@@ -28,32 +27,33 @@ import { ROUTE_PATHES } from '../app.routes';
   templateUrl: './menu-screen.component.html',
   styleUrl: './menu-screen.component.scss',
 })
-export class MenuScreenComponent implements OnInit {
+export class MenuScreenComponent {
   private readonly router = inject(Router);
   private readonly apiService = inject(ApiService);
   private readonly quizService = inject(QuizService);
+  private readonly authService = inject(AuthService);
 
-  private readonly quizScreenPath = `/${ROUTE_PATHES.QUIZ}`;
-  private readonly settingsScreenPath = `${ROUTE_PATHES.SETTINGS}`;
+  public isAdmin$ = of(this.authService.isAdmin);
 
-  public topicsLoadingState$!: Observable<LoadingState<Topic[]>>;
-
-  public ngOnInit(): void {
-    this.topicsLoadingState$ = toLoadingStateStream<Topic[]>(
-      this.apiService.getTopics(),
-    );
-  }
-
-  public goToQuizScreen(): void {
-    this.router.navigate([this.quizScreenPath]);
-  }
+  public topicsLoadingState$ = toLoadingStateStream<Topic[]>(
+    this.apiService.getTopics(),
+  );
 
   public isStartButtonDisabled(): boolean {
     const quizParams = this.quizService.getQuizParams();
     return quizParams?.topic === undefined || quizParams?.count === undefined;
   }
 
+  public goToQuizScreen(): void {
+    this.router.navigate([ROUTE_PATHES.QUIZ]);
+  }
+
   public goToSettingsScreen(): void {
-    this.router.navigate([this.settingsScreenPath]);
+    this.router.navigate([ROUTE_PATHES.SETTINGS]);
+  }
+
+  public goToEditQuestionsScreen(): void {
+    const quizParams = this.quizService.getQuizParams();
+    this.router.navigate([ROUTE_PATHES.ADMIN_QUESTIONS, quizParams.topic]);
   }
 }
