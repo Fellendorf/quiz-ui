6192ab9e2387291a3aae7b95168c168b0df38e2c
Fellendorf <finn85fellendorf@gmail.com>
2024-10-25 03:10:09 +0300

    Prevent "landscape" orientation on mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'quiz-ui';
+export class AppComponent implements OnInit {
+  public isMessageDisplayed = false;
+
+  public ngOnInit() {
+    this.isMessageDisplayed = this.shouldMessageBeDisplayed();
+  }
+
+  @HostListener('window:orientationchange', ['$event'])
+  private onOrientationChange(event: Event) {
+    this.isMessageDisplayed = this.shouldMessageBeDisplayed();
+  }
+
+  private shouldMessageBeDisplayed() {
+    return (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+        navigator.userAgent,
+      ) && /landscape/i.test(screen.orientation.type)
+    );
+  }
 }
